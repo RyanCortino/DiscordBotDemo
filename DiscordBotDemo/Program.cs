22fdd89d9d665c02b95bbbf9822f738bb1f745f0
RyanCortino <RyanCortino@gmail.com>
2022-02-12 11:39:52 -0600

    Add project files.

---
+++ DiscordBotDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Setting up configuartion for Serilog
+        ConfigurationBuilder builder = new ConfigurationBuilder();
+        BuildConfig(builder);
+
+        Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(builder.Build())
+            .Enrich.FromLogContext()
+            .WriteTo.Console()
+            .CreateLogger();
+
+        Log.Logger.Information("Application starting..");
+
+        // Setting up Dependency Injections, Configuration, and Logging
+        IHost host = Host.CreateDefaultBuilder()
+            .ConfigureServices((context, services) =>
+            {
+                // Services go here
+            })
+            .UseSerilog()
+            .Build();
+    }
+
+    static void BuildConfig(IConfigurationBuilder builder)
+    {
+        builder.SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{ Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Productions"}.json", optional: true)
+            .AddEnvironmentVariables();
+    }
+}
