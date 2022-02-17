71d914ac8d4bcea0f565508c4dc14908b63ad524
RyanCortino <RyanCortino@gmail.com>
2022-02-17 09:38:09 -0600

    Added the Command Handler class

--- DiscordBotDemo/Program.cs
+++ DiscordBotDemo/Program.cs
@@ -1,45 +1,65 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Discord.Interactions;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 using System;
 using System.IO;
 
-class Program
+namespace aberrantGeek.DiscordBot
 {
-    static void Main(string[] args)
+    class Program
     {
-        // Setting up configuartion for Serilog
-        ConfigurationBuilder builder = new ConfigurationBuilder();
-        BuildConfig(builder);
+        static void Main(string[] args)
+        {
+            // Setting up configuartion for Serilog
+            ConfigurationBuilder builder = new ConfigurationBuilder();
+            BuildConfig(builder);
 
-        Log.Logger = new LoggerConfiguration()
-            .ReadFrom.Configuration(builder.Build())
-            .Enrich.FromLogContext()
-            .WriteTo.Console()
-            .CreateLogger();
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(builder.Build())
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
 
-        Log.Logger.Information("Application starting..");
+            Log.Logger.Information("Application starting..");
 
-        // Setting up Dependency Injections, Configuration, and Logging
-        IHost host = Host.CreateDefaultBuilder()
-            .ConfigureServices((context, services) =>
-            {
-                // Services go here
-                services.AddTransient<IDiscordBot, DemoDiscordBot>();
-            })
-            .UseSerilog()
-            .Build();
+            // Setting up Dependency Injections, Configuration, and Logging
+            IHost host = Host.CreateDefaultBuilder()
+                .ConfigureServices((context, services) =>
+                {
+                    // Services go here
+                    services.AddSingleton<DiscordSocketClient>();
+                    services.AddSingleton(x => new InteractionService(x.GetRequiredService<DiscordSocketClient>()));
+                    services.AddSingleton<CommandHandler>();
+                    services.AddSingleton<DemoDiscordBot>();
+                })
+                .UseSerilog()
+                .Build();
 
-        IDiscordBot DiscordSvc = host.Services.GetRequiredService<IDiscordBot>();
-        DiscordSvc.Run();
-    }
+            ICustomService discordBot = host.Services.GetRequiredService<DemoDiscordBot>();
+            discordBot.Run();
 
-    static void BuildConfig(IConfigurationBuilder builder)
-    {
-        builder.SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-            .AddJsonFile($"appsettings.{ Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Productions"}.json", optional: true)
-            .AddEnvironmentVariables();
+            ICustomService commandHandler = host.Services.GetRequiredService<CommandHandler>();
+            commandHandler.Run();
+        }
+
+        static void BuildConfig(IConfigurationBuilder builder)
+        {
+            builder.SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{ Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Productions"}.json", optional: true)
+                .AddEnvironmentVariables();
+        }
+
+        public static bool IsDebug()
+        {
+#if DEBUG
+            return true;
+#else
+            return false;
+#endif
+        }
     }
 }
--- DiscordBotDemo/interfaces/IDiscordBot.cs
+++
@@ -1,4 +0,0 @@
-﻿public interface IDiscordBot
-{
-    void Run();
-}
